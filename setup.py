0cb25fcde1f2785295586815d919df1a23820a30
Arghya Banerjee <arghya.banerjee.dev@gmail.com>
2025-12-11 12:01:20 +0530

    Project Setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="Medical Chatbot LLM",
+    version="0.1.0",
+    author="Arghya Banerjee",
+    author_email="arghya.banerjee.dev@gmail.com",
+    packages=find_packages(),
+    install_requires=[
+    ]
+)
